d7e3bc71dd5c61ef85f0d52f977377aea0b63484
Lucas <paradisi.lucas@gmail.com>
2022-05-19 19:36:00 -0300

    Reading every item

--- src/hn_who_is_hiring_scrapper/core.clj
+++ src/hn_who_is_hiring_scrapper/core.clj
@@ -1,28 +1,39 @@
 (ns hn-who-is-hiring-scrapper.core
   (:require [hickory.core :as h]
             [hickory.zip :as z]
+            [hickory.render :as r]
             [clojure.zip :as zip]
             [hickory.select :as s]
             [clj-http.client :as client]
             [clojure.string :as str]))
 
 
-(defn items [content]
-  {:pre [
-         (= 200 (:status content))
-         (not (str/blank? (:body content)))
-         ]
-   }
-  (->> content
-       :body
-       h/parse
-       h/as-hickory
-       (s/select
-         (s/descendant
-           (s/class "comment-tree")
-           (s/tag "tbody")))
+(defn read-item [item]
+  (->> item
+       (s/select (s/class "comment"))
        first
-       :content
-       (filter map?)
-       ))
+       r/hickory-to-html))
+
+
+(defn items [content]
+  {:pre [(= 200 (:status content))
+         (not (str/blank? (:body content)))]}
+  (->>
+    (str/replace (:body content) #"</div></div>" "</div>")
+    h/parse
+    h/as-hickory
+    (s/select
+      (s/descendant
+        (s/class "comment-tree")
+        (s/tag "tbody")))
+    first
+    :content
+    (filter map?)
+    (filter #(= "athing comtr" (:class (:attrs %))))
+    (map read-item)
+    ))
+
+
+(def a (client/get "https://news.ycombinator.com/item?id=31235968&p=1"))
+(items a)
 
--- test/hn_who_is_hiring_scrapper/core_test.clj
+++ test/hn_who_is_hiring_scrapper/core_test.clj
@@ -1,15 +1,21 @@
 (ns hn-who-is-hiring-scrapper.core-test
   (:require [clojure.test :refer :all]
+            [clojure.string :as str]
             [hn-who-is-hiring-scrapper.core :refer :all]
             [hn-who-is-hiring-scrapper.webpage :as w]))
 
 
 (deftest test-items-from-content
   (let [results (items w/webpage)]
-    (is (= 252 (count results)))))
+    (is (= 248 (count results)))))
+
 (deftest test-items-from-empty-content
   (is (thrown? java.lang.AssertionError (items {:status 200 :body ""}))))
+
 (deftest test-items-with-status-error
   (is (thrown? java.lang.AssertionError (items {:status 500 :body "<html></html>"}))))
 
 
+(deftest test-read-comment
+  (is  (= true (str/starts-with? (first (items w/webpage)) "<div class=\"comment\">")))
+  )
