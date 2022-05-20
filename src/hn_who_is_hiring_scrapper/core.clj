4e1f5513d837b7172511e280cad0a872d218fdbc
Lucas <paradisi.lucas@gmail.com>
2022-05-20 00:57:23 -0300

    Removing non usd currencies

--- src/hn_who_is_hiring_scrapper/core.clj
+++ src/hn_who_is_hiring_scrapper/core.clj
@@ -2,26 +2,47 @@
   (:require [hickory.core :as h]
             [hickory.render :as r]
             [hickory.select :as s]
+            [java.util.regex.Pattern]
             [clj-http.client :as client]
             [clojure.string :as str]))
 
 (def SALARY-RANGES [150 400])
 (def BASE-URL "https://news.ycombinator.com/%s")
+(def NON-USD-CURRENCIES "£|€|EUR")
+(def NON-USD-REGEX (-> "(%s)\\ *\\d{2,3}\\ *K|(%s)\\ *\\d{2,3}\\ *-\\ *\\d{2,3}\\ *K)"
+                       (format NON-USD-CURRENCIES NON-USD-CURRENCIES)
+                       java.util.regex.Pattern/quote
+                       re-pattern))
 
 (defn retrieve-all-pages [page-id]
   (loop [href (format "item?id=%s" page-id)
          contents []]
     (println "=> GET " href)
-    (let [raw-content (client/get (format BASE-URL href))
-          content (->> raw-content :body h/parse h/as-hickory)
-          new-href (->> content (s/select (s/class "morelink")) first :attrs :href)]
+    (let [raw-content (-> BASE-URL (format href) client/get)
+          new-href (->> raw-content
+                        :body
+                        h/parse
+                        h/as-hickory
+                        (s/select (s/class "morelink"))
+                        first
+                        :attrs
+                        :href)]
       (if (nil? new-href)
         (conj contents raw-content)
         (recur new-href (conj contents raw-content))))))
 
 (defn read-item [item]
-  (when (zero? (->> item (s/select (s/class "ind")) first :attrs :indent Integer/parseInt))
-    (->> item (s/select (s/class "comment")) first r/hickory-to-html)))
+  (when (->> item
+             (s/select (s/class "ind"))
+             first
+             :attrs
+             :indent
+             Integer/parseInt
+             zero?)
+    (->> item
+         (s/select (s/class "comment"))
+         first
+         r/hickory-to-html)))
 
 (defn concat-items [coll item]
   (conj coll "<tr><td>" item "<hr width=\"90%\" color=\"green\"></tr></td>"))
@@ -69,6 +90,12 @@
   (->> (-> item str/upper-case (str/replace #"401K" ""))
        (re-seq #"\d{3}K") seq))
 
+(defn non-usd-salary? [item]
+  (->> item
+       str/upper-case
+       (re-seq NON-USD-REGEX)
+       seq))
+
 (defn main [page-id exclude-list include-list file-path]
   (let [exclude-list (map str/upper-case exclude-list)
         include-list (map str/upper-case include-list)
@@ -77,6 +104,7 @@
                    (map items)
                    (apply concat)
                    (filter has-salary?)
+                   (remove non-usd-salary?)
                    (remove #(exclude-item? exclude-list %))
                    (filter #(include-item? include-list %))
                    (sort-by max-salary >)
